6842dedca85316351d8e012cc7cb65e943764885
Mario Gomes <mario.mgomes1@gmail.com>
2019-11-04 10:18:08 -0500

    first commit

---
+++ ToDoList/ToDoList/LandingPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoList
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LandingPage : TabbedPage
+    {
+        public LandingPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_OnClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ToDoList/ToDoList/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToDoList
+{
+   
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            bool isEmailBlank = string.IsNullOrEmpty(userName.Text);
+            bool isPasswordBlank = string.IsNullOrEmpty(password.Text);
+
+
+            if (isEmailBlank || isPasswordBlank)
+            {
+                DisplayAlert("LoginError", "Email or Password is missing", "ok");
+            }
+
+            else
+            {
+                Navigation.PushAsync(new LandingPage());
+            }
+        }
+    }
+}
